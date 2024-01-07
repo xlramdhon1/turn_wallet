57966f34c16ac3c2c9958ae665d37be6a253c224
ramdhon1 <ramdhon1.ssss@gmail.com>
2024-01-07 17:27:14 +0700

    menambahkan text pada spalsh screen dan update pada pubspec.yaml

--- lib/screen/spalsh_screen.dart
+++ lib/screen/spalsh_screen.dart
@@ -10,6 +10,26 @@ class SplashScreen extends StatefulWidget {
 class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        width: double.infinity,
+        decoration: const BoxDecoration(color: Color(0xff748DA6)),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset('assets / TurnWallet.png'),
+            const SizedBox(height: 10),
+            const Text(
+              "Turn",
+              style: TextStyle(fontSize: 40),
+            ),
+            const Text(
+              "Wallet",
+              style: TextStyle(fontSize: 40),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
